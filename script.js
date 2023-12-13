40f09f41afc5aa07e193c8afb27468a13acb0a33
Fotynia <114238811+Fotynia@users.noreply.github.com>
2023-12-13 02:09:05 +0200

    п*ята домашка

---
+++ script.js
@@ -0,0 +1,49 @@
+// reverseArray
+// Створіть функцію reverseArray, яка приймає масив і
+//  повертає новий масив, елементи якого розташовані у 
+//  зворотному порядку.
+// function reverseArray() {
+// const originalArray = [1, 2, 3, 4, 5]; 
+// const reversedArray = reverseArray(originalArray);
+// for (let i = originalArray.length - 1; i >= 0; i--) { 
+//     reversedArray += originalArray[i]; 
+//     return reversedArray;
+// }
+// }
+
+
+// uniqueValues
+// Створіть функцію uniqueValues,
+//  яка приймає два масиви і повертає новий масив,
+//   що містить тільки унікальні значення 
+//   з обох масивів (без дублікатів).
+// function uniqueValues () {
+// const array1 = [1, 2, 3, 4, 5]; 
+// const array2 = [3, 4, 5, 6, 7]; 
+// const uniqueValuesArray = array1.concat(array2); 
+// return  uniqueArray = [...new Set(uniqueValuesArray)];
+// }
+
+
+
+// calculateAverageGrade
+// Напишіть функцію calculateAverageGrade, 
+// яка приймає на вхід масив об'єктів з інформацією 
+// про студентів (ім'я, вік, середній бал) і повертає 
+// середній бал усіх студентів.
+
+// function calculateAverageGrade() {
+// const students = [ 
+// { name: "Alice", age: 20, grade: 4.5 }, 
+// { name: "Bob", age: 21, grade: 3.9 }, 
+// { name: "Charlie", age: 19, grade: 4.8 } 
+// ]; 
+// let grades = students.map(student => student.grade);
+// let sum = 0;
+// for (let i = 0; i < grades.length; i++) {
+//     sum += grades[i];
+//         }
+//         let averageResult = sum / grades.length;
+// console.log(averageResult);
+//     return averageResult
+// }
